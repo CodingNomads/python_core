68680e6a49a3a29a64f9db33973f33e9c971c205
martin-martin <breuss.martin@gmail.com>
2019-02-18 22:36:27 +0800

    restructure repo

--- week_01/labs/00_intro/00_03_programmer.py
+++ week_01/labs/00_intro/00_03_programmer.py
@@ -1,8 +1,8 @@
 '''
 Write the necessary code to display the follow message to the console
+Challenge: write code to duplicate the "co-" part
+instead of writing it 3 times.
 
-Hello World!!
-I'm a programmer now.
-Yeehaw!
+Time for co-co-co-ding.
 
 '''
