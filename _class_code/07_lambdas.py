037a4d8fbd94195ebaf6744d5f9d7f5946095970
martin-martin <breuss.martin@gmail.com>
2019-07-31 09:48:23 +0930

    Fix: variable naming

--- _class_code/07_lambdas.py
+++ _class_code/07_lambdas.py
@@ -8,16 +8,16 @@ def square(x):
 print(square(2))
 
 
-_list = [1, 2, 3]
+list_ = [1, 2, 3]
 
 # you can use lambda functions as "throwaway" functions - something
 # that you use only once and then discard, e.g.:
-res_lam = list(map(lambda x: x**2, _list))
-res_fun = list(map(square, _list))  # same thing can be done with a named func
+res_lam = list(map(lambda x: x**2, list_))
+res_fun = list(map(square, list_))  # same thing can be done with a named func
 print(res_lam)
 print(res_fun)
 
 # you can also reproduce lambda's functionality with a list comprehension
-# res_lam = list(map(lambda x: x**2, _list))  # is the same as:
-res = list(x**2 for x in _list)
+# res_lam = list(map(lambda x: x**2, list_))  # is the same as:
+res = list(x**2 for x in list_)
 print(res)
